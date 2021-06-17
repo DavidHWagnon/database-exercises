5d0d88a206cbb0cab4a35bbe964666331593aa33
David Wagnon <david.wagnon02@gmail.com>
2021-06-17 16:21:52 -0500

    Completed MYSQL Tables Exercise

---
+++ albums_migration.sql
@@ -0,0 +1,14 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums
+(
+    id           INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist       VARCHAR(50) DEFAULT 'NONE',
+    name         VARCHAR(100) NOT NULL,
+    release_date INT,
+    sales        DECIMAL (15, 2),
+    genre        VARCHAR(50),
+    PRIMARY KEY (id)
+);
