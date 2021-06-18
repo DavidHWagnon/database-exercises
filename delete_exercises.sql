c18372fa13c39d5862c362f51f3aea3b2be2ab7a
David Wagnon <david.wagnon02@gmail.com>
2021-06-18 12:05:21 -0500

    Completed Basic Statements Exercise

---
+++ delete_exercises.sql
@@ -0,0 +1,16 @@
+USE codeup_test_db;
+
+# DELETE FROM albums WHERE release_date > 1991;
+#
+# DELETE FROM albums WHERE genre = 'disco';
+#
+# DELETE FROM albums WHERE artist = 'Eagles';
+
+SELECT * FROM albums WHERE release_date > 1991;
+
+SELECT * FROM albums WHERE genre = 'disco';
+
+SELECT * FROM albums WHERE artist = 'Eagles';
+
+
+
--- select_exercises.sql
+++ select_exercises.sql
@@ -1,13 +1,19 @@
 Use codeup_test_db;
 
-SELECT * FROM albums WHERE artist = 'Pink Floyd';
+SELECT 'Pinky Floyd Albums' AS 'Pinky Floyd Albums';
+SELECT * FROM albums  WHERE artist = 'Pink Floyd';
 
+SELECT 'Release date for Sgt. Pepper''s Lonely Hearts Club Band' AS 'Sgt.Pepper\'s Lonely Hearts Club Band Release Date';
 SELECT release_date FROM albums WHERE name = 'Sgt.Pepper\'s Lonely Hearts Club Band';
 
+SELECT 'Nevermind Genre' AS 'Nevermind Genre'
 SELECT genre FROM albums WHERE name = 'Nevermind';
 
+SELECT 'Albums released in the 90s' AS '90s albums';
 SELECT * FROM albums WHERE release_date BETWEEN '1990' AND '1999';
 
+SELECT 'Albums with less than 20m in sales' AS 'Albums with less than 20m in sales';
 SELECT * FROM albums WHERE sales < 20;
 
+SELECT 'Rock Albums' AS 'Rock Albums';
 SELECT * FROM albums WHERE genre = 'rock';
