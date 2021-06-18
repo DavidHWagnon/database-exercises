37b3a6364d981cf6288a57ffe3ccced1811fe002
David Wagnon <david.wagnon02@gmail.com>
2021-06-18 10:41:53 -0500

    Completed Insert section of basic statements exercises

--- albums_migration.sql
+++ albums_migration.sql
@@ -4,11 +4,11 @@ DROP TABLE IF EXISTS albums;
 
 CREATE TABLE albums
 (
-    id           INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist       VARCHAR(50) DEFAULT 'NONE',
-    name         VARCHAR(100) NOT NULL,
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(50),
+    name VARCHAR(100) NOT NULL,
     release_date INT,
-    sales        DECIMAL (15, 2),
-    genre        VARCHAR(50),
+    sales DECIMAL (15, 2),
+    genre VARCHAR(50),
     PRIMARY KEY (id)
 );
